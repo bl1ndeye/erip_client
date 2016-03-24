1c052b80108b19c35d04911ef8bf1a56231e20d4
winter <winter@debian-ide.ide>
2016-03-24 15:39:11 +0300

    добавлены комментарии, также изменена работа с логом модуля демона.

--- erip_client/erip_daemon.cpp
+++ erip_client/erip_daemon.cpp
@@ -1,20 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * File:   erip_daemon.cpp
+ * Author: winter
+ *
+ * Created on 23 ноября 2015 г., 20:11
  */
+
+
 
 #include "erip_daemon.h"
 
-void erip_daemon::sighandler(int signum) {
-    waitpid(0, 0, WNOHANG);
-}
+
 
 
 void erip_daemon::setpidfile()
 {
+    // создание пид файла
     FILE* f;
-
     f = fopen("/var/run/erip_client", "w+");
     if (f)
     {
@@ -24,6 +26,8 @@ void erip_daemon::setpidfile()
 }
 erip_daemon::erip_daemon()
 {
+    
+    // конструктор по умолчанию
   erip_ftp ftp_lsv();
   abills_mysql abills_lsv();
  
@@ -32,7 +36,7 @@ erip_daemon::erip_daemon()
 
 bool erip_daemon::reconfigure() {
 
-    
+    // создание xml документа
    pugi::xml_node conf= config.append_child("erip_client_config");
    conf.append_attribute("ftp_ip");
    conf.append_attribute("ftp_login");
@@ -42,6 +46,9 @@ bool erip_daemon::reconfigure() {
    conf.append_attribute("abills_db_login");
    conf.append_attribute("abills_db_password");
  
+   
+   // ввод настроек авторизации на фтп и на mysql сервере
+   // где хранится база данных биллинга
    cout<<"введите ip ftp-сервера"<<endl; 
    string ftp_ip;
    cin>>ftp_ip;
@@ -68,11 +75,13 @@ bool erip_daemon::reconfigure() {
    cin>>abills_db_password;   
 
    
-   unsigned  char *key_lv=(unsigned char *)"123123";
    
-   // дохера всякой шифровки
+   
+   
    conf.attribute("ftp_ip").set_value(ftp_ip.c_str());
    conf.attribute("ftp_login").set_value(ftp_login.c_str());
+   // при вводе пароля на фтп , можно ввести 123 , чтобы 
+   // сохранился пустой пароль.
    if (ftp_password!="123")
    {
     conf.attribute("ftp_password").set_value(ftp_password.c_str());
@@ -80,7 +89,7 @@ bool erip_daemon::reconfigure() {
    conf.attribute("abills_db_ip").set_value(abills_db_ip.c_str());
    conf.attribute("abills_db_login").set_value(abills_db_login.c_str());
    conf.attribute("abills_db_password").set_value(abills_db_password.c_str());
-
+   // сохранение файла конфигурации
    return  config.save_file("erip_config.xml");
    
    
@@ -92,7 +101,7 @@ bool erip_daemon::read_config() {
  
   if (res)
   {
-      //  дохера всякой расшифровки
+      // чтение конфигурации
       pugi::xml_node conf= config.first_child();
       string pass_ftp=conf.attribute("ftp_password").as_string();
       
@@ -107,7 +116,6 @@ bool erip_daemon::read_config() {
           //  счетчик для исключения бесконечной
          // рекурсии
         rec_count++;
-        
         reconfigure();
         if (rec_count<=2)
         read_config();  
@@ -126,27 +134,37 @@ bool erip_daemon::read_config() {
 void erip_daemon::daemonize() {
     pid_t pid;
     pid_t sid;
+    // форк приложения
     pid = fork();
-
     if (pid<0)
     {
+        // при ошибке форка закрыть приложение
         exit(EXIT_FAILURE);
     } else
         if (pid>0)
         {
+            // получен новый pid, следовательно приложение уже запущено
+            // закрывается, что бы не было два экзэмпляра
             exit(EXIT_SUCCESS);
         }
             
             sid=setsid();
             if (sid < 0) { exit(EXIT_FAILURE); }
+            // закрытие потока вывода
             close(1); 
+            // изменение прав доступа к файлам
             umask(0);
+            // открытие ведения лога
             openlog("erip_daemon", 0, LOG_LOCAL0);
             syslog(LOG_INFO,"демон запущен");
             setpidfile();
+            closelog();
             abills_lsv.Set_Pref(pref);
+            // запуск бесконечного цикла.
+            // в цикле выполняются все функции автоматизации
             while (true)
                 {
+                openlog("erip_daemon", 0, LOG_LOCAL0);
                 if (ftp_lsv.establish_connection())
                      syslog(LOG_INFO,"соединение с фтп прошло");
                 else
@@ -164,6 +182,10 @@ void erip_daemon::daemonize() {
                 now_tm = localtime(&now);
                 hour = now_tm->tm_hour;             
                 int min =now_tm->tm_min;
+                // проверка времени, для формирования 
+                // платежного требования
+                // сообщение 202 по стандарту ЕРИП
+                // фомируется и сбрасывается в интервале от 01:00:00 и 01:14:59
                 if (hour==1 && min<15 )
                 {
                    if ( ftp_lsv.create_payment_request(abills_lsv.Get_USER_ID_DEPOSIT_List()))
@@ -184,6 +206,9 @@ void erip_daemon::daemonize() {
                    }
                     
                 }
+                // если по какой-то причине файл платежного требования был создан
+                // но не был закачан в итерацию создания файла
+                // то он будет загружен 
                 if (ftp_lsv.pr_w84upload)
                 {
                       if ( ftp_lsv.put_payment_request())
@@ -194,7 +219,7 @@ void erip_daemon::daemonize() {
                           syslog(LOG_INFO," ошибка закачки платежного требования на сервер");
                       };
                 }
-        
+                // получение сообщений о платежах, а также рузультатов обработки сообщений 202
                 if ( ftp_lsv.get_payment_regs())
                    {
                        syslog(LOG_INFO,"были получены сообщения о платежах");
@@ -206,24 +231,18 @@ void erip_daemon::daemonize() {
                 abills_lsv.Disconnect();
                 
                 sleep(900);
-                
+                closelog();
                 }
-                
-                
             
-            
-            
-            //mainloop();
-            closelog();
 
-
-        //};
    
 }
 
 
 int erip_daemon::run(string pre)
 {
+    // инициализации объектов фтп и mysql
+    // запуск демонизации 
     pref=pre;    
     ftp_lsv.Set_Pref(pref);
     daemonize();
